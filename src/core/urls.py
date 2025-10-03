033500f9ee6f6ffd023ee2a6bce77ca990a2cfdf
curtisnlogan <curtisnlogan@gmail.com>
2025-10-03 17:44:02 +0100

    refactor: remove core app index template to base.html and update HomePage view to use base template

--- src/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import HomePage
-
-app_name = 'core'
-
-urlpatterns = [
-    path('', HomePage.as_view(), name='home'),
-]
--- src/core/views.py
+++ src/core/views.py
@@ -3,5 +3,5 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 
-class HomePage(TemplateView):
-    template_name = 'core/index.html'
+class Base(TemplateView):
+    template_name = 'base.html'
