a43cf73ccf289e8703086abde1d6c7d60a744863
curtisnlogan <curtisnlogan@gmail.com>
2025-10-03 17:45:13 +0100

    refactor: update URL configuration to use Base view directly and remove include for core.urls

--- src/lodestar/urls.py
+++ src/lodestar/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from core .views import Base
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("core.urls")),
+    path('', Base.as_view(), name='home'),
 ]
