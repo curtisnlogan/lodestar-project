dcf2290baa9198b970a61d7f72ecf00c2f869538
curtisnlogan <curtisnlogan@gmail.com>
2025-10-05 19:32:17 +0100

    feat: include allauth URLs for user authentication

--- src/lodestar/urls.py
+++ src/lodestar/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from core .views import Base
+from django.urls import path, include
+from core.views import Base
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', Base.as_view(), name='home'),
 ]
