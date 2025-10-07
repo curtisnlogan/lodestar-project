a78efe9214c771483f84d964631c272f957fa1c2
curtisnlogan <curtisnlogan@gmail.com>
2025-10-07 20:10:44 +0100

    refactor: update Home view import to use observations app

--- src/lodestar/urls.py
+++ src/lodestar/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from core.views import Home
+from observations.views import Home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', Home.as_view(), name='home')
+    path('', Home.as_view(), name='home'),
 ]
