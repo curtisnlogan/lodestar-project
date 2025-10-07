f4482170248b99f13481600524627ecb739e3895
curtisnlogan <curtisnlogan@gmail.com>
2025-10-07 20:06:25 +0100

    refactor: remove Home view class from core app, move to observations app

--- src/core/views.py
+++ src/core/views.py
@@ -1,14 +1,3 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from allauth.account.forms import LoginForm
 
 # Create your views here.
-
-# simply renders home.html for testing purposes
-class Home(TemplateView):
-    template_name = 'home.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = LoginForm()
-        return context
