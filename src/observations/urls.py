dda12a2ac04a9488fba33ecfa5c6584bbc6913b6
curtisnlogan <curtisnlogan@gmail.com>
2025-10-09 20:03:31 +0100

    feat: add observation list, detail, and delete routes to URL configuration

--- src/observations/urls.py
+++ src/observations/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path
-from .views import AddObservationView
+from .views import (
+    AddObservationView,
+    ObservationListView,
+    ObservationDetailView,
+    DeleteObservationView,
+)
 
 urlpatterns = [
-    path('add/', AddObservationView.as_view(), name='add_observation'),
+    path("add/", AddObservationView.as_view(), name="add_observation"),
+    path("list/", ObservationListView.as_view(), name="observation_list"),
+    # use unique PK present for each entry in an observation model, to build a unique url
+    path(
+        "detail/<str:obs_type>/<int:obs_id>/",
+        ObservationDetailView.as_view(),
+        name="observation_detail",
+    ),
+    path(
+        "delete/<str:obs_type>/<int:obs_id>/",
+        DeleteObservationView.as_view(),
+        name="delete_observation",
+    ),
 ]
